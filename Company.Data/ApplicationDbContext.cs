8e1140e801efa08688eee2286fb8cb97e4aa6bc0
HazemSerdah <61620915+001hazem@users.noreply.github.com>
2022-02-06 12:51:58 +0200

    Add project files.

---
+++ Company.Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Company.Data
+{
+    public class madDbContext : IdentityDbContext
+    {
+        public madDbContext(DbContextOptions<madDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
